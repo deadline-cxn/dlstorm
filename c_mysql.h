cae17bfa8fb956ec3dcf4fd68a8010c2310b8acf
sethcoder <defectiveseth@gmail.com>
2013-08-14 23:01:53 -0400

    mysql

--- c_mysql.h
+++ c_mysql.h
@@ -20,3 +20,14 @@
  ******************************************************************************/
 #include "dlstorm.h"
 #include <mysql.h>
+class C_MySQL {
+public:
+    C_MySQL(char *host, char *user, char *pass, char *db);
+    ~C_MySQL();
+
+    MYSQL *con;
+    MYSQL_RES * Query(char *q);
+    MYSQL_ROW Fetch_Object(MYSQL_RES *result);
+
+
+};
